2f4137a264615ed5e6df025fe1347d21703bda9d
Admin <amritadebbarman@gmail.com>
2018-12-29 15:22:01 +0530

    Added

--- PythonBasics/14.ReadInReversedOrder.py
+++
@@ -1,8 +0,0 @@
-#   wap to read the content of a file in reverse order
-class ReadFileInReverse:
-    def readInReverse(self):
-        with open("readdata.txt","r") as file:
-            for line in reversed(list(file)):
-                print(line.rstrip())
-ob=ReadFileInReverse()
-ob.readInReverse()
